6b8be0aa2f2c064b154097fae79cc7569de0381b
Cecile-Hirschauer <59025247+Cecile-Hirschauer@users.noreply.github.com>
2020-10-11 16:34:43 +0200

    Project : Getting Ready for Physics Class
    Turn up the Temperature
    1. Write a function called f_to_c that takes an input f_temp, a temperature in Fahrenheit, and converts it to c_temp, that temperature in Celsius. It should then return c_temp.
    The equation you should use is: Temp (C) = (Temp (F) - 32) * 5/9
    
    2. Let’s test your function with a value of 100 Fahrenheit. Define a variable f100_in_celsius and set it equal to the value of f_to_c with 100 as an input.
    
    3. Write a function called c_to_f that takes an input c_temp, a temperature in Celsius, and converts it to f_temp, that temperature in Fahrenheit. It should then return f_temp.
    The equation you should use is: Temp (F) = Temp (C) * (9/5) + 32
    
    4. Let’s test your function with a value of 0 Celsius. Define a variable c0_in_fahrenheit and set it equal to the value of c_to_f with 0 as an input.
    
    Use the Force
    
    5. Define a function called get_force that takes in mass and acceleration. It should return mass multiplied by acceleration.
    
    6. Test get_force by calling it with the variables train_mass and train_acceleration. Save the result to a variable called train_force and print it out.
    
    7. Print the string “The GE train supplies X Newtons of force.”, with X replaced by train_force.
    
    8. Define a function called get_energy that takes in mass and c. c is a constant that is usually set to the speed of light, which is roughly 3 x 10^8. Set c to have a default value of 3*10**8. get_energy should return mass multiplied by c squared.
    
    9. Test get_energy by using it on bomb_mass, with the default value of c. Save the result to a variable called bomb_energy.
    
    10. Print the string “A 1kg bomb supplies X Joules.”, with X replaced by bomb_energy.
    
    Do the Work
    
    11. Define a final function called get_work that takes in mass, acceleration, and distance. Work is defined as force multiplied by distance. First, get the force using get_force, then multiply that by distance. Return the result.
    
    12. Test get_work by using it on train_mass, train_acceleration, and train_distance. Save the result to a variable called train_work.
    
    13. Print the string "The GE train does X Joules of work over Y meters.", with X replaced with train_work and Y replaced with train_distance.

---
+++ Getting_Ready_for_Physics_Class.py
@@ -0,0 +1,64 @@
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+
+bomb_mass = 1
+
+#Turn up the Temperature
+def f_to_c(f_temp):
+  """
+  Convert temperature in Farenheit to temperature in Celsius
+  """
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+
+def c_to_f(c_temp):
+  """
+  Convert temperature in Celsius to temperature in Farenheit
+  """
+  f_temp = c_temp * (9/5) + 32
+  return f_temp 
+
+f100_in_celsius = f_to_c(100)
+c0_in_fahrenheit = c_to_f(0)
+print(f100_in_celsius)
+print(c0_in_fahrenheit)
+
+print('---------------------------------------------')
+
+# Use the Force
+def get_force(mass, acceleration):
+  """
+  Return mass multiplied by acceleration
+  """
+  return mass * acceleration
+
+
+def get_energy(mass, c = 3*10**8):
+  """
+  c is a constant that is usually set to the speed of light, which is roughly 3 x 10^8. 
+  Return mass multiplied by c squared.
+  """
+  return mass * c ** 2
+
+print('----------------------------------------------')
+
+# Do the Work
+def get_work(mass, acceleration, distance):
+  """
+  Work is defined as force multiplied by distance. First, get the force using get_force, then multiply that by distance.
+  """
+  return get_force(mass, acceleration) * distance
+
+train_force = get_force(train_mass, train_acceleration)
+print('The GE train supplies {} Newtons of force.'.format(train_force))
+
+bomb_energy = get_energy(bomb_mass)
+print('A 1kg bomb supplies {} Joules.'.format(bomb_energy))
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print('The GE train does {} Joules of work over {} meters.'.format(train_work, train_distance))
+
+
+
+
